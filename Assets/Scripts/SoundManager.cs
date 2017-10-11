9c11300fe2385ae61a99ba10099c412e4caa37c8
StivArts <jaimearansoto@gmail.com>
2017-10-11 13:18:45 -0300

    Adding files to repository

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -26,16 +26,52 @@ public class SoundManager : MonoBehaviour
     [SerializeField]
 
     public AudioTrack[] audioTracks;
-
+    public AudioSource source;
     // Use this for initialization
     void Start()
     {
+        XMLManager.Serializar(audioTracks, "Assets/newFile.xml");
         audioTracks = XMLManager.Deserializar<AudioTrack[]>("Assets/tracks.xml");
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
+    public AudioClip getClip(int index)
+    {
+        AudioClip clip = Resources.Load<AudioClip>(audioTracks[index].url);
+        return clip;
+    }
+    public AudioClip getClip(string name)
+    {
+
+        Debug.Log(getAudioTrack(name).url);
+        AudioClip clip = Resources.Load(getAudioTrack(name).url) as AudioClip;
+
+        if (clip == null)
+        {
+            Debug.Log("Audio Clip not found");
+        }
+        return clip;
+    }
+    public AudioTrack getAudioTrack(string name)
+    {
+        for (int i = 0; i < audioTracks.Length; i++)
+        {
+            if (audioTracks[i].name.Equals(name))
+            {
+                return audioTracks[i];
+            }
+        }
+        return null;
+    }
+    public void playTrack(string name)
+    {
+        source.clip = getClip(name);
+        source.loop = getAudioTrack(name).loop;
+        source.Play();
 
     }
 }
